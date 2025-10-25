c94975ad2523fa6bd2885f37b6231697cabdbe29
Alessandro Benassi <127418373+ale-bena@users.noreply.github.com>
2025-10-25 22:20:07 +0200

    Add files via upload

---
+++ model_teacher_bn.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+from tensorflow.keras import layers, Model
+
+def conv_block(x, filters):
+    x = layers.Conv2D(filters, 3, padding='same', use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+
+    x = layers.Conv2D(filters, 3, padding='same', use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+
+    return x
+
+def encoder_block(x, filters):
+    c = conv_block(x, filters)
+    p = layers.MaxPooling2D((2, 2))(c)
+    return c, p
+
+def decoder_block(x, skip, filters):
+    x = layers.Conv2DTranspose(filters, 2, strides=2, padding='same')(x)
+    x = layers.Concatenate()([x, skip])
+    x = conv_block(x, filters)
+    return x
+
+def build_blur_unet(input_shape=(128, 128, 3)):
+    # Input
+    inputs = layers.Input(shape=input_shape)
+
+    # Encoder
+    c1, p1 = encoder_block(inputs, 32)
+    c2, p2 = encoder_block(p1, 64)
+    c3, p3 = encoder_block(p2, 128)
+
+    # Bottleneck
+    bn = conv_block(p3, 256)
+
+    # Decoder
+    d3 = decoder_block(bn, c3, 128)
+    d2 = decoder_block(d3, c2, 64)
+    d1 = decoder_block(d2, c1, 32)
+
+    # Output
+    outputs = layers.Conv2D(3, (1, 1), activation='sigmoid')(d1)
+
+    return Model(inputs, outputs)
