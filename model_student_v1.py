14b6f813ec909cdcf2ac2de76c4bc1cb681014ac
Alessandro Benassi <127418373+ale-bena@users.noreply.github.com>
2025-10-31 20:48:50 +0100

    Update and rename model_student_v2.py to model_student_v1.py

--- model_student_v1.py
+++ model_student_v1.py
@@ -2,8 +2,14 @@ import tensorflow as tf
 from tensorflow.keras import layers, Model
 
 def conv_block(x, filters):
-    x = layers.Conv2D(filters, 3, padding='same', activation='relu')(x)
-    x = layers.Conv2D(filters, 3, padding='same', activation='relu')(x)
+    x = layers.Conv2D(filters, 3, padding='same', use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+
+    x = layers.Conv2D(filters, 3, padding='same', use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+
     return x
 
 def encoder_block(x, filters):
@@ -18,7 +24,8 @@ def decoder_block(x, skip, filters):
     return x
 
 def build_blur_unet(input_shape=(128, 128, 3)):
-    inputs = layers.Input(input_shape)
+    # Input
+    inputs = layers.Input(shape=input_shape)
 
     # Encoder
     c1, p1 = encoder_block(inputs, 16)
@@ -33,6 +40,7 @@ def build_blur_unet(input_shape=(128, 128, 3)):
     d2 = decoder_block(d3, c2, 32)
     d1 = decoder_block(d2, c1, 16)
 
+    # Output
     outputs = layers.Conv2D(3, (1, 1), activation='sigmoid')(d1)
 
     return Model(inputs, outputs)
